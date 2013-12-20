7cfdf0e08824de888f3d5ac969ed525a6f9db356
Mudar Noufal <mn@mudar.ca>
2013-12-19 19:48:37 -0500

    Help activity animation
    Added themes values file

--- FairphonePeaceOfMind/src/main/java/ca/mudar/fairphone/peaceofmind/ui/HelpActivity.java
+++ FairphonePeaceOfMind/src/main/java/ca/mudar/fairphone/peaceofmind/ui/HelpActivity.java
@@ -18,7 +18,6 @@
 package ca.mudar.fairphone.peaceofmind.ui;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -36,7 +35,6 @@ public class HelpActivity extends Activity {
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), PeaceOfMindActivity.class));
                 finish();
             }
         });
